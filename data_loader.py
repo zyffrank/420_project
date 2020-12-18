488631013843c2df2f5f72cec9e8bc81e9c0382b
zhangyunfan <frankyunfanzhang@gmail.com>
2020-12-17 20:05:56 -0500

    changes

--- data_loader.py
+++ data_loader.py
@@ -6,54 +6,42 @@ from torchvision import transforms
 # https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 # Load test data
 class TestDataLoader(Dataset):
-    def __init__(self, input_images, output):
-        self.input_images = input_images
-        self.output = output
+    def __init__(self, images, labels):
+        self.images = images
+        self.labels = labels
         self.transform = transforms.ToTensor()
         self.trans = transforms.Grayscale()
 
     def __len__(self):
-        return len(self.input_images)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        image = self.input_images[idx]
-        output = self.output[idx]
-        image = self.transform(image)
-        return [image, output]
+        return [self.transform(self.images[idx]), self.labels[idx]]
 
 
 # Load training data
 class TrainLoader(Dataset):
-    def __init__(self, input_images, output):
-
-        train_images = input_images[200:]
-        train_labels = output[200:]
-        self.input_images, self.output = train_images, train_labels
+    def __init__(self, images, label):
+        self.images, self.labels = images[200:], label[200:]
         self.transform = transforms.ToTensor()
 
     def __len__(self):
-        return len(self.input_images)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        image = self.input_images[idx]
-        output = self.output[idx]
-        image = self.transform(image)
-        return [image, output]
+        return [self.transform(self.images[idx]), self.labels[idx]]
 
 
 # Load validation data
 class ValidationLoader(Dataset):
-    def __init__(self, input_images, output):
-        self.input_images = input_images[0:200]
-        self.output = output[0:200]
+    def __init__(self, images, labels):
+        self.images = images[0:200]
+        self.labels = labels[0:200]
         self.transform = transforms.ToTensor()
 
     def __len__(self):
-        return len(self.input_images)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        image = self.input_images[idx]
-        output = self.output[idx]
-        image = self.transform(image)
-        return [image, output]
+        return [self.transform(self.images[idx]), self.labels[idx]]
 
